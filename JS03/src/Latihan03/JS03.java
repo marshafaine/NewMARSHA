37c3a572053bae0023ec1a26ea1c6de417a69fb0
marshafaine <145307117+marshafaine@users.noreply.github.com>
2023-10-25 16:58:54 +0700

    Marsha faine s_22343057_Jobsheet 3_PBO Prak

---
+++ JS03/src/Latihan03/JS03.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Latihan03;
+
+/**
+ *
+ * @author ACER
+ */
+public class JS03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[]args) {
+        // TODO code application logic here
+        int a;
+        a=10;
+        short s;
+        s= 2;
+        byte b;
+        b = 6;
+        long l = 23456789;
+        float f;
+        f = 65.20298f;
+        double d;
+        d = 876.765d;
+        System.out.println("The integer variabel is " + a);
+        System.out.println("The short variabel is " + s);
+        System.out.println("The byte variabel is " + b);
+        System.out.println("The long variabel is " + l);
+        System.out.println("The float variabel is " + f);
+        System.out.println("The double variabel is " + d);
+    }
+    
+}
---
+++ JS03/src/Tugas03/tugas03.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas03;
+
+/**
+ *
+ * @author ACER
+ */
+public class tugas03 {
+   public static void main(String[] args) {
+       int number = 10;
+       char letter = 'a';
+       boolean result = true;
+       String str = "hello";
+       
+       System.out.println("Number = " + number);
+       System.out.println("letter = " + letter);
+       System.out.println("result = " + result);
+       System.out.println("str = " + str);
+   } 
+}
